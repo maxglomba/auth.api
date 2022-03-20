ab62e2dd3dd53f02adc7459234110ab787c30c33
maxglomba <46114159+maxglomba@users.noreply.github.com>
2022-03-19 21:13:27 -0300

    update mock data

--- src/common/persistence/mock.persistence.ts
+++ src/common/persistence/mock.persistence.ts
@@ -2,8 +2,8 @@ const db = {
     users: [
         {
             id: 1,
-            email: "maximiliano.glomba@gmail.com",
-            password: "k36NX7tIvUlJU2zWW401xCa4DS+DDFwwjizexCKuIkQ=",
+            email: 'testuser@gmail.com',
+            password: 'k36NX7tIvUlJU2zWW401xCa4DS+DDFwwjizexCKuIkQ=',
             created_at: new Date('2022-03-19 22:21:31.607'),
             updated_at: null
         }
