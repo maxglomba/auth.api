75c725131e19b17e77326c224ea49594bf50cbf6
maxglomba <46114159+maxglomba@users.noreply.github.com>
2022-03-21 15:13:51 -0300

    fix normalize error

--- src/common/controllers/base.controller.ts
+++ src/common/controllers/base.controller.ts
@@ -1,9 +1,9 @@
 import { Response } from 'express';
-import { ApplicationException } from "../exceptions/application.exception";
+import { ApplicationExceptions } from '../exceptions/application.exception';
 
 export abstract class BaseController {
     handleException(err: any, res: Response) {
-        if (err instanceof ApplicationException) {
+        if (err instanceof ApplicationExceptions) {
             res.status(400);
             res.send(err.message);
         } else {
