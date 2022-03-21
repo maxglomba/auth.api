38db46ccf0d24b393dd4d892c71fc8d712e59ac0
maxglomba <46114159+maxglomba@users.noreply.github.com>
2022-03-21 15:14:26 -0300

    implementation swagger

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,27 @@ import express, { json } from 'express';
 import { loadControllers } from 'awilix-express';
 import container from './container';
 
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
+
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Tasks API',
+            version: '1.0.0',
+            description: 'A simple express library API'
+        },
+        servers: [
+            {
+                url: 'http://localhost:5000'
+            }
+        ]
+    },
+    apis: ['./src/controllers/*.ts']
+        
+};
+
 // Create a new express app instance
 const app: express.Application = express();
 
@@ -19,6 +40,9 @@ app.use(json());
 
 // Load dependencies
 container(app);
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 // Load controllers
 app.use(loadControllers(
--- src/controllers/identity.controller.ts
+++ src/controllers/identity.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { route, POST, GET } from 'awilix-express';
 import { IdentityService } from '../services/identity.service';
 import { BaseController } from '../common/controllers/base.controller';
-import { UserCreateDto } from '../dtos/user.dto';
+import { UserPayloadDto } from '../dtos/user.dto';
 
 @route('/identity')
 export default class IdentityController extends BaseController {
@@ -10,6 +10,72 @@ export default class IdentityController extends BaseController {
         super();
     }
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ * 
+ *   User:
+ *    type: object
+ *    properties:
+ *     id: 
+ *      type: string
+ *      description: the auto-generated id of user
+ *     email:
+ *      type: string
+ *      description: the name of the user
+ *     created_at:
+ *      type: date | null
+ *      description: the date when the user was created
+ *     updated_at:
+ *      type: date | null
+ *      description: the date when the user was updated
+ *    required:
+ *     - email
+ *    example:
+ *     id: 1
+ *     email: testuser@gmail.com
+ *     created_at: 2022-03-20T01:21:31.607Z
+ *     updated_at: null
+ * 
+ *   UserPayload:
+ *    type: object
+ *    properties:
+ *     email:
+ *      type: string
+ *      description: the name of the user
+ *     password:
+ *      type: string
+ *      description: the password to the user
+ *    required:
+ *     - email
+ *     - password
+ *    example:
+ *     email: testuser@gmail.com
+ *     password: test1234
+ *    responses:
+ *     200:
+ *      description: Success
+*/
+
+/**
+ * @swagger
+ * /identity/users:
+ *  get:
+ *   summary: Get all users registered
+ *   tags: [Users]
+ *   responses:
+ *    200:
+ *     description: the list of users
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: array
+ *        items:
+ *         $ref: '#/components/schemas/User'
+ *         
+ *         
+ */
     @route('/users')
     @GET()
     async all(req: Request, res: Response) {
@@ -17,6 +83,27 @@ export default class IdentityController extends BaseController {
         res.send(result);
     }
 
+/**
+ * @swagger
+ * /identity/authenticate:
+ *  post:
+ *   description: Authenticate user in platform
+ *   requestBody:
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/UserPayload'
+ *   responses:
+ *    200:
+ *     description: Success
+ *     content:
+ *      text/plain:
+ *       example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJ0ZXN0dXNlckBnbWFpbC5jb20iLCJpYXQiOjE2NDc4ODU3MjUsImV4cCI6MTY0NzkxMDkyNX0.VKVLsYkBTl0HX4pMY0UYxc7hu2kaUflQWom1vC0iOPI
+ *    401:
+ *     description: Unauthorized
+ *    400:
+ *     description: Bad Request
+ */
     @route('/authenticate')
     @POST()
     async index(req: Request, res: Response) {
@@ -29,7 +116,27 @@ export default class IdentityController extends BaseController {
             this.handleException(error, res);
         }
     }
-
+/**
+ * @swagger
+ * /identity/create:
+ *  post:
+ *   description: Create user in platform
+ *   requestBody:
+ *     content:
+ *      application/json:
+ *       schema:
+ *         $ref: '#/components/schemas/UserPayload'
+ *   responses:
+ *    204:
+ *     description: Success
+ *    400:
+ *     description: Invalid Email
+ *     content:
+ *      text/plain:
+ *       type: string
+ *       description: Invalid email message
+ *       example: Email already exists.
+ */
     @route('/create')
     @POST()
     async create(req: Request, res: Response) {
@@ -37,7 +144,7 @@ export default class IdentityController extends BaseController {
             await this.identityService.create({
                 email: req.body.email,
                 password: req.body.password
-            } as UserCreateDto);
+            } as UserPayloadDto);
 
             res.status(204);
             res.send();
