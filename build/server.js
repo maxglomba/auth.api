5fc36a6ac9d3e064342af27c3fc5d08d6e1d7d71
maxglomba <46114159+maxglomba@users.noreply.github.com>
2022-04-21 22:41:52 -0300

    update with build

--- build/server.js
+++ build/server.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-app_1.app.listen(5000, function () {
-    console.log('App is running on port 5000!');
+app_1.app.listen(process.env.PORT || 5000, function () {
+    console.log('App is running on port' + process.env.PORT + '!');
 });
