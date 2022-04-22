073314d845621d33d08e21365e0702d0417f3e87
maxglomba <46114159+maxglomba@users.noreply.github.com>
2022-04-21 22:27:09 -0300

    fix port config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { app } from './app';
 
-app.listen(5000, function () {
-    console.log('App is running on port 5000!');
+app.listen(process.env.PORT || 5000, function () {
+    console.log('App is running on port' + process.env.PORT + '!');
 });
